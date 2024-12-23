92fab9e258b7da33fa8f082b4ebdf996e394d9ec
Ant0in <antoine.berthion@ulb.be>
2024-12-23 02:26:40 +0100

    proper main + logger

--- main.py
+++ main.py
@@ -1,49 +1,69 @@
 
 import numpy as np
-import matplotlib.pyplot as plt
-
 from src.AutoEncoder import AutoEncoder
 from src.utils import *
+import argparse
 
 
 
 class MainExecutor:
 
+    def __init__(self, logger: Logger) -> None:
+        self.logger: Logger = logger  # Logger idk cool shit
+
+    def readDataset(self, dataset_fp: str) -> np.ndarray:
+        self.logger.info(msg=f'Reading dataset @ {dataset_fp}.')
+        x_, _ = get_dataset(filepath=dataset_fp)
+        self.logger.info(msg=f'Finished reading dataset.')
+        return x_
+
+    def initializeAutoEncoder(self, x_vector: np.ndarray, learning_rate: float, encoded_dim: int) -> AutoEncoder:
+        # Generates an AutoEncoder based on the dataset written in x_vector and the parameters.
+        self.logger.info(msg=f'Initializing AutoEncoder with parameters : (learning_rate={learning_rate}, encoded_dim={encoded_dim}).')
+        ae: AutoEncoder = AutoEncoder(input_dim=x_vector.shape[1], encoded_dim=encoded_dim, learning_rate=learning_rate)
+        self.logger.info(msg=f'AutoEncoder initialized.')
+        return ae
+    
+    def train(self, encoder: AutoEncoder, x_vector: np.ndarray, batch_size: int, epochs: int) -> list[float]:
+        # Trains 'encoder' AutoEncoder with 'x_vector', and returns the losses vector.
+        self.logger.info(msg=f'Starting training for encoder with parameters : (epochs={epochs}, batch_size={batch_size})')
+        losses: list[float] = encoder.train(x_train=x_vector, epochs=epochs, batch_size=batch_size)
+        self.logger.info(msg=f'Training complete for encoder.')
+        self.logger.info(msg=f'Losses over epochs ({epochs}) : {[float(round(l, 4)) for l in losses]}')
+        return losses
+
+
+
+class Argparser:
+
     @staticmethod
-    def main() -> None:
+    def parseArgs() -> dict:
+        parser = argparse.ArgumentParser(description="AutoEncoder Training Script")
+        parser.add_argument("--dataset", type=str, required=True, help="Dataset path (ex: './dataset/mnist_train.csv').")
+        parser.add_argument("--learning_rate", type=float, default=0.01, help="Learning rate for autoencoder (default : 0.01).")
+        parser.add_argument("--encoded_dim", type=int, default=64, help="Encoded vector dimension (default : 64).")
+        parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training (default : 32).")
+        parser.add_argument("--epochs", type=int, default=10, help="Number of epochs for training (default : 10).")
+        parser.add_argument("--verbose", action="store_true", default=True, help="Set logger verbose flag (default : true)")
+        parser.add_argument("--log_path", type=str, default=None, help="Log file path (ex: './logs/logs.log').")
+        return vars(parser.parse_args())
+    
 
-        x_train, _ = get_dataset("./dataset/mnist_train.csv")
-        x_test, _ = get_dataset("./dataset/mnist_test.csv")
 
-        # Déclaration de l'encodeur ;
-        LR: float = 0.01
-        encoder: AutoEncoder = AutoEncoder(input_dim=x_train.shape[1], encoded_dim=64, learning_rate=LR)
-
-        # Paramètres d'entraînement ;
-        epochs: int = 10
-        batch_size: int = 32
-        losses: list[float] = encoder.train(x_train=x_train, epochs=epochs, batch_size=batch_size)
-
-        displayImages: int = 5
-
-        plt.figure(figsize=(10, displayImages))
-        for i in range(displayImages):
-            v = x_test[i]
-            ax = plt.subplot(2, displayImages, i + 1)
-            plt.imshow(np.reshape(v, (28, 28)), cmap="gray")
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
-            decoded_v = encoder.forward(v)
-            ax = plt.subplot(2, displayImages, i + 1 + displayImages)
-            plt.imshow(np.reshape(decoded_v, (28, 28)), cmap="gray")
-            plt.gray()
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
-
-        plt.show()
+def main() -> list[float]:
+    parameters: dict = Argparser.parseArgs()
+    executor: MainExecutor = MainExecutor(logger=Logger(filepath=parameters['log_path'], verbose=parameters['verbose']))
+    x_train: np.ndarray = executor.readDataset(dataset_fp=parameters['dataset'])
+    encoder: AutoEncoder = executor.initializeAutoEncoder(x_vector=x_train,
+        learning_rate=parameters['learning_rate'], encoded_dim=parameters['encoded_dim'])
+    losses: list[float] = executor.train(encoder=encoder, x_vector=x_train, batch_size=parameters['batch_size'], epochs=parameters['epochs'])
+    return losses
 
 
 if __name__ == "__main__":
 
-    MainExecutor.main()
+    # run examples : 
+    # >> python .\main.py --dataset .\dataset\mnist_train.csv --learning_rate 0.01 --batch_size 32 --epochs 10
+
+    _ = main()
 
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
+from datetime import datetime
+
+
+
+class Logger:
+
+    def __init__(self, filepath: str | None = None, verbose: bool = False):
+        
+        self._verbose: bool = verbose
+        self._filepath: str | None = filepath
+
+    @property
+    def verbose(self) -> bool:
+        return self._verbose
+    
+    @property
+    def filepath(self) -> str | None:
+        return self._filepath
+    
+    def getTimestamp(self) -> str:
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def writeInFile(self, msg: str) -> None:  
+        if not (self.filepath is None):
+            # could keep the file descriptor somewhere but idk I'll pass
+            with open(file=self.filepath, mode='a', encoding='utf-8') as f:
+                f.write(f'{msg}\n')
+
+    def printInSTDOUT(self, msg: str) -> None:
+        if self.verbose: print(msg)
+
+    def logMessage(self, msg: str) -> None:
+        timestamped_msg: str = f"[{self.getTimestamp()}] {msg}"
+        self.writeInFile(msg=timestamped_msg)
+        self.printInSTDOUT(msg=timestamped_msg)
+
+    def info(self, msg: str) -> None:
+        self.logMessage(msg=f"[i] {msg}")
+
+    def warning(self, msg: str) -> None:
+        self.logMessage(msg=f"[w] {msg}")
+
+    def error(self, msg: str) -> None:
+        self.logMessage(msg=f"[e] {msg}")
+
+    def critical(self, msg: str) -> None:
+        self.logMessage(msg=f"[c] {msg}")
 
 
 
@@ -24,7 +71,6 @@ def get_dataset(filepath: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.n
         - x_train, y_train, x_test, y_test (np.ndarray): the images and labels for training and testing sets.
     """
 
-    print("[i] Reading dataset...")
     d = np.loadtxt(filepath, delimiter=",", dtype=str)[1:].astype(np.int64)
     x, y = d[:, 1:], d[:, 0].T
     return x/255., y
